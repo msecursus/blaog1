7ffea08061a0d7529337d46ba1330269f76cf4f7
mse <mosaelotje@gmail.com>
2016-08-11 14:41:13 +0000

    crudart

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => {registrations: 'registrations' } 
+  resources :articles
+  
   get 'pages/index'
 
   get 'pages/contact'
