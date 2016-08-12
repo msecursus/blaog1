00bf7785ed44a869f3d81c102f67c29f77b382f6
mse <mosaelotje@gmail.com>
2016-08-12 21:13:14 +0000

    image test

---
+++ config/initializers/mail.rb
@@ -0,0 +1,15 @@
+#ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.smtp_settings = {
+#:address => 'smtp.sendgrid.net',
+#:port => '587',
+#:authentication => :plain,
+#:user_name => ENV["SENDGRID_USERNAME"],
+#:password => ENV["SENDGRID_PASSWORD"],
+#:domain => 'heroku.com',
+#:enable_starttls_auto => true
+#}
+
+
+# hier ben ik gestopt . vorige les = compleet . heroku push gedaan en tot hier doorgegaan .ook heroku run rake db:migrate .
+# nu ga ik verder met comments.ik sla dit over, deze blz is niet ge add /commit/pushed ergens naar toe .
+# dus geen reset verg passw,en geen contact page met sendgrid en device.
